db7aa7e8205364368479d6f37fa4a2c390b5dac4
masayuki.katsumi <kendoheiwa@gmail.com>
2019-10-18 16:31:30 +0900

    resubmit

--- SlideshowApp/KakudaiViewController.swift
+++ SlideshowApp/KakudaiViewController.swift
@@ -11,27 +11,11 @@ import UIKit
 class KakudaiViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-     //var sightImage = ["image1": UIImage(named: "path-4518094_1280.jpg"), "image2": UIImage(named: "landscape-4518195_1280.jpg"), "image3": UIImage(named: "volcano-44933461_1280.jpg")]
-    
-    //bigimageの初期値
+
     var bigimage = UIImage(named: "path-4518094_1280.jpg")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //イメージを出力
         imageView.image = bigimage
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,42 +10,27 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    
-    //@IBOutlet weak var timerLabel: UILabel!
-    
     //タイマー用時間のための配列。
     var timer_sec: Float = 0
-    
     var timer :Timer!
     var value: Int = 1
     //UIImageの配列
     var sightImage = ["image1": UIImage(named: "path-4518094_1280.jpg"), "image2": UIImage(named: "landscape-4518195_1280.jpg"), "image3": UIImage(named: "volcano-4493461_1280.jpg")]
-   
-    
-    
     //画像UIImageView
     @IBOutlet weak var imageView: UIImageView!
     //画像をtapして画面を遷移
     @IBAction func tapAction(_ sender: Any) {
         self.performSegue(withIdentifier: "KakudaiViewController", sender: nil)
+        self.timer.invalidate()
     }
     //画面を元に戻す遷移
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        //imageViewにイメージを代入
         let image = sightImage["image\(value)"]
-        //imageViewにimageを入れて表示
         imageView.image = image as? UIImage
-        
-      
     }
-    
-    //timeInterval: 2 repeat:true で指定された通り、2秒ごとに呼び出され続ける関数
     @objc func updateTimer(_ timer: Timer) {
         if value >= 3 {
             value = 1
@@ -56,15 +41,12 @@ class ViewController: UIViewController {
         let image = sightImage ["image\(value)"]
         imageView.image = image as? UIImage
         }
-        
     }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //segueから遷移先のResultViewControllerを取得する
         let kakudaiViewController:KakudaiViewController = segue.destination as! KakudaiViewController
         //遷移先のKakudaiViewControllerで宣言しているbigimageに値を代入して渡す
         kakudaiViewController.bigimage = sightImage["image\(value)"]!
-        
     }
     //進むボタン
     @IBOutlet weak var Advance: UIButton!
@@ -77,7 +59,7 @@ class ViewController: UIViewController {
        value += 1
         let image = sightImage ["image\(value)"]
         imageView.image = image as? UIImage
-    }
+        }
     }
     //戻るボタン
     @IBOutlet weak var back: UIButton!
@@ -93,11 +75,7 @@ class ViewController: UIViewController {
         }
     }
     //再生・停止ボタン
-   
     @IBAction func goAndPause(_ sender: Any) {
-        
-        // タップするとボタンの表示が停止になる
-        //timerのに値がなければ動作開始
         if self.timer == nil {
             self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
             //ボタンを停止ボタンに変更する
@@ -107,21 +85,16 @@ class ViewController: UIViewController {
             Advance.isEnabled = false
             //停止ボタンをタップ可能にする
             back.isEnabled = false
-            
         } else {
         //値があればタイマー停止
             self.timer.invalidate() //タイマーを停止
             self.timer = nil
-            
             let button = sender as! UIButton
             button.setTitle("再生", for: .normal)
-         
             //進むボタンをタップ不可にする
              Advance.isEnabled = true
             //停止ボタンをタップ不可にする
              back.isEnabled = true
+        }
     }
-    
-    }
-
 }
